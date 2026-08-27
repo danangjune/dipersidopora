9ae9ef7678939ee48bcd4b076dcf6f22115c6745
chasanalim <chasanalim07@gmail.com>
2026-08-27 09:35:58 +0700

    fix: update map embedding URLs and handle authorization header in .htaccess

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,7 +12,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600,700,800&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @vite(['resources/css/app.css', 'resources/js/app.jsx'])
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen flex">
